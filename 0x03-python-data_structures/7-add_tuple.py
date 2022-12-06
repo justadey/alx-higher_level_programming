d0376d5361270e6d5be23c148b1c557d85105088
ogedengbe <o.oluwakayode16@gmail.com>
2022-12-05 16:29:35 -0800

    new updates

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        tuple_a += (0, 0)
-    elif len(tuple_a) == 1:
-        tuple_a += (0,)
-    elif len(tuple_a) > 2:
-        tuple_a = tuple_a[:2]
+    a1 = tuple_a + ('0', '0')
+    b1 = tuple_b + ('0', '0')
+
+    sum1 = int(a1[0]) + int(b1[0])
+    sum2 = int(a1[1]) + int(b1[1])
+
+    return sum1, sum2
