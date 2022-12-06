c816da5bd513b3d55f524e7bd54ffe1bb221f836
ogedengbe <o.oluwakayode16@gmail.com>
2022-12-05 16:56:27 -0800

    updated directory

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
-    print("{:d}".format(res))
+    from sys import argv
+
+num1 = 0
+length = len(argv)
+for i in range(1, length):
+    num1 += int(argv[i])
+print('{}'.format(num1))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import sys
     import hidden_4
-    for n in dir(hidden_4):
-        if n[:2] != "__":
-            print(n)
+
+    for name in dir(hidden_4):
+        if name[1] != '_':
+            print(name)
