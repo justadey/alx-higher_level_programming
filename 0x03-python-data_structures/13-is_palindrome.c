bc22f8a32fc7490f4fd2d0d05864b9b22402bdfd
ogedengbe <o.oluwakayode16@gmail.com>
2022-12-05 16:38:14 -0800

    task review

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,22 +1,59 @@
 #include "lists.h"
+
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0 if not, 1 if is
+ * is_palindrome - Check if is palindrome
+ *
+ * @head: Head of Linked List
+ *
+ * Return: 0 if not palindrome, 1 if is palindrome
  */
+
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
-	listint_t *temp;
 
-	if (head == NULL|| *head == NULL)
-		return (1);
-
-	temp = *head;
-	while (temp) /* get len of list */
+	if (!*head || !(*head))
 	{
-		temp = temp->next;
-		len++;
+		return (1);
+	}
+
+	if (p_check(head, *head))
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ *  p_check - Check for palindrome
+ *
+ *  @left: Go left
+ *
+ *  @right: Go right
+ *
+ *  Return: Integer
+ */
+
+
+int p_check(listint_t **left, listint_t *right)
+{
+	int is_p = 0;
+
+	if (right)
+	{
+		is_p = p_check(left, right->next);
+	}
+	else
+	{
+		return (1);
+	}
+
+	if (is_p == 1)
+	{
+		if ((*left)->n == right->n)
+		{
+			(*left) = (*left)->next;
+			return (1);
+		}
 	}
 	return (0);
 }
